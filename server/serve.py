9b062006842da81cfc6cae5bfd8193189b028243
OlgierdIwaniuk <133595200+OlgierdIwaniuk@users.noreply.github.com>
2023-12-05 23:06:38 +0100

    implementacja kodu serwerowego w pythonie

--- server/serve.py
+++ server/serve.py
@@ -1,29 +1,48 @@
-from flask import Flask, request, render_template, jsonify
+import time
+from flask import Flask, request,  jsonify
 
 app = Flask(__name__, template_folder='template')
-gpio_state = {'gpioState': []}
 
-@app.route('/gpio_state', methods=['POST'])
+state={}
+
+@app.route('/hooks', methods=['POST'])
 def receive_data():
+    
+    lastUpdate = time.time()
+    global state
     try:
-        content = request.get_json()
-        received_gpio_state = content['gpioState']
-        gpio_state['gpioState'] = received_gpio_state
+
+        content = request.get_json() 
+        received_state = content['state']
+        board_id = content['board_id']
+        if board_id not in state:
+            state[board_id] = {}
+
+         
+        state[board_id]['state'] = received_state
+        state[board_id]['place'] = content['place']
+        state[board_id]['time'] = lastUpdate
         
-        print(f"Received data: gpio_state={received_gpio_state}")
+        print(f"Board ID: {board_id}, Place: {state[board_id]['place']}, State: {state[board_id]['state']}")
+        
         return jsonify({'success': True})
     except Exception as e:
-        print(f"Error receiving gpio_state: {str(e)}")
+        print(f"Error receiving state: {str(e)}")
         return jsonify({'success': False, 'error': str(e)})
 
-@app.route('/get_gpio_state', methods=['GET'])
-def get_gpio_state():
-    return jsonify(gpio_state)
 
-@app.route('/', methods=['GET'])
-def home():
-    return render_template("index.html", gpio_state=gpio_state)
+@app.route('/hooks', methods=['GET'])
+def check_state():
+    global state
+    for board_id, board_state in state.items():
+        if (time.time() - board_state['time']) > 7200:
+            board_state['state'] = 'offline'
+        elif (time.time() - board_state['time']) > 10:
+            board_state['state'] = 'empty'    
+    
+    
+    return jsonify(state)
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
-    
+    app.run(host="0.0.0.0", debug=True)
